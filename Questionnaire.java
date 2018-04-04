9f7ce2f6f6ffbe5cc66ceaecef0be4d0c2bbc393
Tye Borden <tyeborden@gmail.com>
2018-04-04 15:06:38 -0300

    This is an updated drive class questionnaire

--- Questionnaire.java
+++ Questionnaire.java
@@ -1,31 +1,53 @@
+//Created by T Borden
+// Questionnair.java - Driver class
 
+/**
+ * This is simply the driver class to show how Menu2 is used.
+ * @author T Borden
+ */
 public class Questionnaire
 {
     public static void main(String[] args)
     {
+        //Menu2 created with no parameters
         Menu2 obj = new Menu2();
         obj.display();
         System.out.println();
-        System.out.println(obj.len());
 
-        Menu2 good = new Menu2("The Test");
-        good.addQuestion(
-            "How was your day?",
-            "Good",
-            "Ok",
-            "Better",
+        //Menu2 created with four question added
+        Menu2 math = new Menu2("The Simple Math Test");
+        math.addQuestion(
+            "What is 1 + 2 ?",
+            "3",
+            "4",
+            "5",
             "A");
 
-        good.addQuestion(
-            "Whats your favorite colour?",
-            "Green",
-            "Red",
-            "Orange",
+        math.addQuestion(
+            "What is  3 - 1 ?",
+            "4",
+            "6",
+            "2",
+            "C");
+
+        math.addQuestion(
+            "What is  2 x 2 ?",
+            "7",
+            "4",
+            "2",
             "B");
 
-        good.display();
-        System.out.println(good.len());
-        good.score();
+        math.addQuestion(
+            "What is 9 / 3 ?",
+            "3",
+            "7",
+            "10",
+            "A");
+
+        //Displaying the questions
+        math.display();
+        //Displaying the score
+        math.score();
 
     }
 }
